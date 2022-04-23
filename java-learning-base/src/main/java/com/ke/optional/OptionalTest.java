8bcfab2b12177bb252252238774b00fd0d97ac7e
yangyudong003 <yangyudong003@ke.com>
2022-04-23 12:13:29 +0800

    nothing

--- java-learning-base/src/main/java/com/ke/optional/OptionalTest.java
+++ java-learning-base/src/main/java/com/ke/optional/OptionalTest.java
@@ -1,6 +1,5 @@
 package com.ke.optional;
 
-import jdk.nashorn.internal.runtime.options.Option;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
--- java-learning-base/src/main/java/com/ke/uuid/Test.java
+++ java-learning-base/src/main/java/com/ke/uuid/Test.java
@@ -1,9 +1,5 @@
 package com.ke.uuid;
 
-import sun.security.provider.MD5;
-
-import java.util.UUID;
-
 public class Test {
     public static void main(String[] args) {
 
