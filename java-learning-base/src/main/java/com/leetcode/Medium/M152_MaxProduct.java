fec478bb040f82675a21f9da186d78b23c202375
yangyudong003 <yangyudong003@ke.com>
2022-05-12 01:03:54 +0800

    nothing

--- java-learning-base/src/main/java/com/leetcode/Medium/M152_MaxProduct.java
+++ java-learning-base/src/main/java/com/leetcode/Medium/M152_MaxProduct.java
@@ -44,36 +44,30 @@ public class M152_MaxProduct {
         System.out.println(new M152_MaxProduct().maxProduct(new int[]{-2,0,1,4,0,-2,-3,2,-2}));
     }
 
-    int[] dp, dn; // 动态规划
-
     public int maxProduct(int[] nums) {
         if (nums.length == 1) return nums[0];
+
         // 维护俩dp数组
         // dp[i] = 以第i个元素结尾的子数组最大正数乘积
         // dn[i] = 以第i个元素结尾的子数组最小负数乘积
-        dp = new int[nums.length];
-        dn = new int[nums.length];
+        int[] dp = new int[nums.length];
+        int[] dn = new int[nums.length];
+        int max = nums[0];
         // 初始化第一个元素
         dp[0] = Math.max(nums[0], 0);
         dn[0] = Math.min(nums[0], 0);
         // 从第二个元素开始遍历，每个元素都利用前一个元素的dp和dn计算出自己的值
         for (int i = 1; i < nums.length; i++) {
-            maxProduct(nums, i);
-        }
-        // 找出正数dp里的最大值
-        int max = Integer.MIN_VALUE;
-        for (int i : dp) {
-            max = Math.max(max, i);
+            int n = nums[i];
+            // dp[x]的值为 max(自身，自身 * 前面的最大正数，自身 * 前面的最小负数)
+            dp[i] = Math.max(n, Math.max(dp[i - 1] * n, dn[i - 1] * n));
+            // dn[x]的值为 min(自身，自身 * 前面的最大正数，自身 * 前面的最小负数)
+            dn[i] = Math.min(n, Math.min(dp[i - 1] * n, dn[i - 1] * n));
+
+            // 找出正数dp里的最大值
+            max = Math.max(max, dp[i]);
         }
         return max;
-    }
-
-    public void maxProduct(int[] nums, int x) {
-        int n = nums[x];
-        // dp[x]的值为 max(自身，自身 * 前面的最大正数，自身 * 前面的最小负数)
-        dp[x] = Math.max(n, Math.max(dp[x - 1] * n, dn[x - 1] * n));
-        // dn[x]的值为 min(自身，自身 * 前面的最大正数，自身 * 前面的最小负数)
-        dn[x] = Math.min(n, Math.min(dp[x - 1] * n, dn[x - 1] * n));
     }
 
 }
